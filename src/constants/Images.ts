8d7936d18b3f74038f068abbf9873d3c480e34e3
Jaspreet Singh <jaspreetbcoder@gmail.com>
2023-11-03 17:58:08 +0530

    code pushed

--- src/constants/Images.ts
+++ src/constants/Images.ts
@@ -1,7 +1,9 @@
 import UploadIllustration from "assets/images/upload.jpg";
+import AuthBackground from "assets/images/auth-background.gif";
 
 const Images = {
   UPLOAD: UploadIllustration,
+  AUTH_BACKGROUND: AuthBackground,
 };
 
 export default Images;
